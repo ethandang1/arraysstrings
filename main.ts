766209dcf2e308d54404b696849b474e7f466480
ethandang1 <58610660+ethandang1@users.noreply.github.com>
2020-02-27 13:25:19 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let food1 = "sushi"
+let food2 = "pasta"
+let food3 = "rice"
+let food4 = "chicken"
+let food5 = "chips"
+let listFoods = [food1, food3, food5]
+game.splash(listFoods[0] + ", " + listFoods[1] + ", and " + listFoods[2])                                                                                                                                         
